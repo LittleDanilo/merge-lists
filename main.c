639c2a78872a2934fb47cd4f7c1ae63c9ebeb960
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2024-08-23 10:41:47 -0300

    conditional upgrades

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int merge(int v1[], int v2[], int i1, int i2) {
-    if (i1 >= 6 && i2 >= 6) return 0;
-    else if ((v1[i1] < v2[i2] && i1 < 6) || i2 > 5) {
+int merge(int v1[], int v2[], int i1, int i2, int t) {
+    if (i1 >= t && i2 >= t) return 0;
+    if ((v1[i1] < v2[i2] && i1 < t) || i2 >= t) {
         printf("%d ", v1[i1]);
-        merge(v1, v2, i1 + 1, i2);
-    } else if ((v2[i2] < v1[i1] && i2 < 6) || i1 > 5){
+        merge(v1, v2, i1 + 1, i2, t);
+    } else {
         printf("%d ", v2[i2]);
-        merge(v1, v2, i1, i2 + 1);
+        merge(v1, v2, i1, i2 + 1, t);
     }
 }
 
 int main(void) {
-    int v1[6] = {0,1,2,3,4,5};
-    int v2[6] = {10,11,12,13,14,15};
+    int v1[10] = {10,25,30,46,70,73,75,81,83,86};
+    int v2[10] = {9,11,26,32,33,45,50,51,68,90};
 
-    merge(v1, v2, 0, 0);
+    merge(v1, v2, 0, 0, 10);
     printf("\n");
     return 0;
 }
